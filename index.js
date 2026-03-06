aaadf8308055fadbdede11fc6d02792937e43c2a
Lauren Peabody <lauren.peabody@nhrec.org>
2026-03-05 21:47:05 -0800

    Added more styling and embedded map

---
+++ index.js
@@ -0,0 +1,27 @@
+/*** Dark Mode ***
+  Purpose:
+  - Use this starter code to add a dark mode feature to your website.
+***/
+
+
+// Step 1: Select the theme button
+const themeButton = document.getElementById("theme-button");
+
+// Step 2: Write the callback function
+const toggleDarkMode = () => {
+    // Write your code here
+    // This section will run whenever the button is clicked
+    document.body.classList.toggle("dark-mode");
+}
+
+
+// Step 3: Register a 'click' event listener for the theme button,
+//             and tell it to use toggleDarkMode as its callback function
+themeButton.addEventListener("click", toggleDarkMode);
+
+
+
+/*** Form Handling [PLACEHOLDER] ***/
+/*** Form Validation [PLACEHOLDER] ***/
+/*** Animations [PLACEHOLDER] ***/
+/*** Success Modal [PLACEHOLDER] ***/
